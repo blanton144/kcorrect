3d210a0f39ff434d6818f63e9705c84ae8d8b888
Michael R. Blanton <michael.blanton@gmail.com>
2024-05-10 21:14:58 -0400

    template updates

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,10 @@
 
 import os
 import sys
-
-import autoapi
 from importlib.metadata import version
 
 # Define path to the code to be documented **relative to where conf.py (this file) is kept**
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -31,12 +29,25 @@ extensions = ["sphinx.ext.mathjax", "sphinx.ext.napoleon", "sphinx.ext.viewcode"
 extensions.append("autoapi.extension")
 extensions.append("nbsphinx")
 
-templates_path = []
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
+# -- sphinx-copybutton configuration ----------------------------------------
+extensions.append("sphinx_copybutton")
+## sets up the expected prompt text from console blocks, and excludes it from
+## the text that goes into the clipboard.
+copybutton_exclude = ".linenos, .gp"
+copybutton_prompt_text = ">> "
 
-master_doc = "index"  # This assumes that sphinx-build is called from the root directory
-html_show_sourcelink = False  # Remove 'view source code' from top of page (for html, not python)
-add_module_names = False # Remove namespaces from class/method signatures
+## lets us suppress the copy button on select code blocks.
+copybutton_selector = "div:not(.no-copybutton) > div.highlight > pre"
+
+templates_path = []
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+
+# This assumes that sphinx-build is called from the root directory
+master_doc = "index"
+# Remove 'view source code' from top of page (for html, not python)
+html_show_sourcelink = False
+# Remove namespaces from class/method signatures
+add_module_names = False
 
 autoapi_type = "python"
 autoapi_dirs = ["../src"]
--- docs/index.rst
+++ docs/index.rst
@@ -7,8 +7,38 @@
 
 .. rubric:: :header_no_toc:`kcorrect documentation`
 
+<<<<<<< before updating
 Overview
 =========================
+=======
+.. code-block:: console
+
+   >> conda create env -n <env_name> python=3.10
+   >> conda activate <env_name>
+
+
+Once you have created a new environment, you can install this project for local
+development using the following commands:
+
+.. code-block:: console
+
+   >> pip install -e .'[dev]'
+   >> pre-commit install
+   >> conda install pandoc
+
+
+Notes:
+
+1) The single quotes around ``'[dev]'`` may not be required for your operating system.
+2) ``pre-commit install`` will initialize pre-commit for this local repository, so
+   that a set of tests will be run prior to completing a local commit. For more
+   information, see the Python Project Template documentation on
+   `pre-commit <https://lincc-ppt.readthedocs.io/en/latest/practices/precommit.html>`_.
+3) Installing ``pandoc`` allows you to verify that automatic rendering of Jupyter notebooks
+   into documentation for ReadTheDocs works as expected. For more information, see
+   the Python Project Template documentation on
+   `Sphinx and Python Notebooks <https://lincc-ppt.readthedocs.io/en/latest/practices/sphinx.html#python-notebooks>`_.
+>>>>>>> after updating
 
 This page has documentation for the kcorrect product to calculate
 astronomical K-corrections for galaxies.
